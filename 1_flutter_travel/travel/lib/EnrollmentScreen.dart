32461f5498f159271ce7454edb2992b820f9581c
ssearobber <ssearobber@naver.com>
2019-12-10 22:17:47 +0900

    20191210

--- 1_flutter_travel/travel/lib/EnrollmentScreen.dart
+++ 1_flutter_travel/travel/lib/EnrollmentScreen.dart
@@ -17,17 +17,41 @@ class _EnrollmentScreenState extends State<EnrollmentScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.white, elevation: 0.0),
       body: Padding(
         padding: EdgeInsets.all(16.0),
         child: Form(
           key: this._formKey,
           child: ListView(
             children: <Widget>[
-              TextFormField(
+              new TextFormField(
+                  decoration: InputDecoration(
+                      labelText: 'name', border: OutlineInputBorder()),
+                  maxLength: 12,
+                  maxLengthEnforced: true),
+              new SizedBox(height: 13.0),
+              new Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  new Radio(
+                    value: 0,
+                  ),
+                  new Text('male'),
+                  new Padding(
+                    padding: EdgeInsets.all(8.0),
+                  ),
+                  new Radio(
+                    value: 1,
+                  ),
+                  new Text('female')
+                ],
+              ),
+              new SizedBox(height: 30.0),
+              new TextFormField(
                 decoration: InputDecoration(
-                    labelText: 'name', border: OutlineInputBorder()),
-                maxLength: 12,
-              )
+                    labelText: 'description', border: OutlineInputBorder()),
+                maxLength: 60,
+              ),
             ],
           ),
         ),
